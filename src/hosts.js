9929992167e11e7c6a495eca76ae5c7fdf592010
Thomas Weber <weber_t@mailbox.org>
2020-11-02 18:14:36 -0600

    Handle private hosts.js errors better

--- src/hosts.js
+++ src/hosts.js
@@ -5,8 +5,12 @@ if (process.env.NODE_ENV !== 'test') {
     const hosts = require('../hosts.js');
     module.exports = hosts;
   } catch (e) {
-    // ignore
-  }  
+    if (e && typeof e === 'object' && e.code !== 'MODULE_NOT_FOUND') {
+      console.error('could not read private hosts file');
+      console.error(e);
+      process.exit(1);
+    }
+  }
 }
 
 module.exports = {
