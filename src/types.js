d679ed28ca797a0b47bb7c3dd5f97facbc7753fb
Thomas Weber <weber_t@mailbox.org>
2020-12-27 13:44:14 -0600

    Add .webmanifest

--- src/types.js
+++ src/types.js
@@ -21,6 +21,9 @@ module.exports = {
   '.map': {
     type: 'application/json'
   },
+  '.webmanifest': {
+    type: 'application/json'
+  },
   '.svg': {
     type: 'image/svg+xml',
     encodings: [br, gz]
