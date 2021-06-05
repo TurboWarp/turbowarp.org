c6cad61bc82cbe30441c477172af56bbaa8d3ec3
Thomas Weber <weber_t@mailbox.org>
2021-06-05 10:45:45 -0500

    Increase blocks-media max-age to 1 week

--- src/server.js
+++ src/server.js
@@ -204,7 +204,7 @@ app.get('*/static/assets/*', (req, res, next) => {
 });
 app.get('*/static/blocks-media/*', (req, res, next) => {
   // File names don't contain hash of content, but these files are hot and will rarely change.
-  res.header('Cache-Control', 'public, max-age=3600, immutable');
+  res.header('Cache-Control', 'public, max-age=604800, immutable');
   next();
 });
 app.get('*', (req, res, next) => {
