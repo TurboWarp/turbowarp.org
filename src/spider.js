b1c530eae6f958668315a2d41b848301654f325f
GarboMuffin <muffin@mailbox.org>
2022-11-03 00:29:33 -0500

    Add more bots to the spider list

--- src/spider.js
+++ src/spider.js
@@ -13,7 +13,14 @@ const isSpider = (userAgent) => {
         userAgent.includes('redditbot') ||
         userAgent.includes('WhatsApp') ||
         userAgent.includes('TelegramBot') ||
-        userAgent.includes('Twitterbot')
+        userAgent.includes('Twitterbot') ||
+        userAgent.includes('Mastodon') ||
+        userAgent.includes('LinkedInBot') ||
+        userAgent.includes('Baiduspider') ||
+        userAgent.includes('DuckDuckBot') ||
+        userAgent.includes('facebookexternalhit') ||
+        userAgent.includes('Applebot') ||
+        userAgent.includes('MojeekBot')
     );
 };
 
