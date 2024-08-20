08ee033675af98d69ff554acc03cce0f9efb4997
Muffin <muffin@muffin.ink>
2024-08-20 12:30:11 -0500

    Fix tests

--- test/types.test.js
+++ test/types.test.js
@@ -5,11 +5,11 @@ const request = supertest(app);
 it('serves html with the proper Content-Type', () => {
   return request.get('/index.html')
     .set('Host', 'localhost')
-    .expect('Content-Type', 'text/html');
+    .expect('Content-Type', 'text/html; charset=utf-8');
 });
 
 it('serves js with the proper Content-Type', () => {
   return request.get('/file.js')
     .set('Host', 'localhost')
-    .expect('Content-Type', 'text/javascript');
+    .expect('Content-Type', 'text/javascript; charset=utf-8');
 });
