8ebd5842f37cb5cf4d0951cab5906c2b572e5bf0
Thomas Weber <muffin@muffin.ink>
2025-11-30 11:27:10 -0600

    Use native fetch instead of node-fetch

--- src/scratch-api.js
+++ src/scratch-api.js
@@ -1,19 +1,13 @@
-const https = require('https');
-const fetch = require('node-fetch');
-
 const API = 'https://trampoline.turbowarp.org/proxy/projects/$id';
-const OPTIONS = {
-    agent: new https.Agent({
-        keepAlive: true
-    }),
-    headers: {
-        referer: 'https://project-og.turbowarp.org'
-    },
-    timeout: 5000
-};
 
 const getProjectMeta = async (id) => {
-    const response = await fetch(API.replace('$id', id), OPTIONS);
+    const response = await fetch(API.replace('$id', id), {
+        keepalive: true,
+        headers: {
+            referer: 'https://project-og.turbowarp.org'
+        },
+        signal: AbortSignal.timeout(5000)
+    });
     if (response.status === 404) {
         throw new Error('Project is unshared');
     }
